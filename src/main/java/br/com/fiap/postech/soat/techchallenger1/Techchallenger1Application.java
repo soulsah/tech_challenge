eee86eff910e98b6b7655bf072ad1df54a5f9b8e
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2023-10-29 16:48:37 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/postech/soat/techchallenger1/Techchallenger1Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.soat.techchallenger1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techchallenger1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Techchallenger1Application.class, args);
+	}
+
+}
