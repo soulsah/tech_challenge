267cfe066c1289146c1acb2216df702643fc3548
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 <guilherme_oelias@hotmail.com>
2023-12-09 17:48:43 -0300

    feat: Controller Cliente

--- src/main/java/br/com/fiap/postech/soat/techchallenger1/adapters/api/ClienteController.java
+++ src/main/java/br/com/fiap/postech/soat/techchallenger1/adapters/api/ClienteController.java
@@ -1,4 +1,17 @@
 package br.com.fiap.postech.soat.techchallenger1.adapters.api;
 
+import br.com.fiap.postech.soat.techchallenger1.adapters.dto.ClienteDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class ClienteController {
+
+    @PostMapping("/cliente")
+    public ResponseEntity<ClienteDto> cadastroCliente(@RequestBody ClienteDto cliente) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
+    }
 }
---
+++ src/main/java/br/com/fiap/postech/soat/techchallenger1/adapters/dto/ClienteDto.java
@@ -0,0 +1,5 @@
+package br.com.fiap.postech.soat.techchallenger1.adapters.dto;
+
+
+public record ClienteDto(String cpf, String nome, String email) {
+}
