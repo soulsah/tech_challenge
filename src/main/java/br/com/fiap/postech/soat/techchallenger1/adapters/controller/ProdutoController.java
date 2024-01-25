bfcb2ba57b9dafac749f39383a1b0fadb954b037
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2024-01-25 19:42:59 -0300

    feat: atualizando documentação swagger

--- src/main/java/br/com/fiap/postech/soat/techchallenger1/adapters/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/postech/soat/techchallenger1/adapters/controller/ProdutoController.java
@@ -3,6 +3,9 @@ package br.com.fiap.postech.soat.techchallenger1.adapters.controller;
 import br.com.fiap.postech.soat.techchallenger1.adapters.dto.ProdutoDto;
 import br.com.fiap.postech.soat.techchallenger1.application.ProdutoService;
 import br.com.fiap.postech.soat.techchallenger1.domain.model.Produto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,11 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
 
+    @Operation(summary = "Busca todos os produtos",
+            description = "Busca todos os produtos")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+    })
     @GetMapping
     public ResponseEntity<List<ProdutoDto>> findAll(){
         List<Produto> produtos = produtoService.findAll();
@@ -24,17 +32,24 @@ public class ProdutoController {
         return ResponseEntity.ok().body(produtoDtos);
     }
 
+    @Operation(summary = "Busca produto por id",
+            description = "Busca produto por id do produto")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ProdutoDto> findProdutoById(@PathVariable("id") Long produto_id){
-        /*
-         * TODO:
-         *  Tratar exceções 404
-         */
+
         Produto produto = produtoService.findProdutoById(produto_id);
         ProdutoDto produtoDto = new ProdutoDto(produto);
         return ResponseEntity.ok().body(produtoDto);
     }
 
+    @Operation(summary = "Busca produto por tipo",
+            description = "Busca produto por tipo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+    })
     @GetMapping("/tipo/{id}")
     public ResponseEntity<List<ProdutoDto>> findProdutosByTipo(@PathVariable("id") Long tipo){
         List<Produto> produtos = produtoService.findProdutosByTipo(tipo);
@@ -42,12 +57,23 @@ public class ProdutoController {
         return ResponseEntity.ok().body(produtoDtos);
     }
 
+    @Operation(summary = "Cadastra novo produto",
+            description = "Cadastra novo produto")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Busca realizada com sucesso"),
+    })
     @PostMapping
     public ResponseEntity<?> cadastroProduto(@RequestBody ProdutoDto produto){
         produtoService.cadastrarProduto(produto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Produto cadastrado com sucesso!");
     }
 
+
+    @Operation(summary = "Atualiza  produto com base no id",
+            description = "Atualiza  produto com base no id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Busca realizada com sucesso"),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<?> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDto produto){
         Produto produtoExistente = produtoService.findProdutoById(id);
@@ -63,6 +89,11 @@ public class ProdutoController {
         return ResponseEntity.ok(produtoExistente);
     }
 
+    @Operation(summary = "exclui  produto com base no id",
+            description = "exclui  produto com base no id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Busca realizada com sucesso"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> excluirProduto(@PathVariable Long id){
         Produto produtoExistente = produtoService.findProdutoById(id);
