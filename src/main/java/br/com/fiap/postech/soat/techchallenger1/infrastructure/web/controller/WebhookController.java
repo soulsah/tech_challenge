0db002c7c1686336ee9259ed432bd5453a1a6a92
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2024-03-10 18:22:50 -0300

    feat: atualizando pagamento webhook

--- src/main/java/br/com/fiap/postech/soat/techchallenger1/infrastructure/web/controller/WebhookController.java
+++ src/main/java/br/com/fiap/postech/soat/techchallenger1/infrastructure/web/controller/WebhookController.java
@@ -1,7 +1,10 @@
 package br.com.fiap.postech.soat.techchallenger1.infrastructure.web.controller;
 
 
+import br.com.fiap.postech.soat.techchallenger1.application.exception.PagamentoNaoEncontradoException;
+import br.com.fiap.postech.soat.techchallenger1.application.service.PagamentoService;
 import br.com.fiap.postech.soat.techchallenger1.infrastructure.web.dto.NotificacaoPagamentoDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/webhook")
 public class WebhookController {
 
+    @Autowired
+    private PagamentoService pagamentoService;
+
     @PostMapping("/payment")
-    public ResponseEntity<String> paymentNotification(@RequestBody NotificacaoPagamentoDto notification){
+    public ResponseEntity<String> paymentNotification(@RequestBody NotificacaoPagamentoDto notification) throws PagamentoNaoEncontradoException {
 
-
+        pagamentoService.updatePaymentStatus(notification);
 
         return new ResponseEntity<>("Notificação de pagamento recebida com sucesso", HttpStatus.OK);
     }
