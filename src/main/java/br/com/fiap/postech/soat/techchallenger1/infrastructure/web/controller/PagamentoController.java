8e8af69f9f99c62166f100a7487faf9a07376478
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2024-03-10 00:15:36 -0300

    feat: arrumando url

--- src/main/java/br/com/fiap/postech/soat/techchallenger1/infrastructure/web/controller/PagamentoController.java
+++ src/main/java/br/com/fiap/postech/soat/techchallenger1/infrastructure/web/controller/PagamentoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/pagamentos")
 public class PagamentoController {
 
 
